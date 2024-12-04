82bcc8437613853736010650af6158ea8baea919
sergeiryzhkov <sergeyryzhkov2001@mail.ru>
2024-12-04 03:04:04 +0300

    Toast message call was created

--- app/src/main/java/com/example/mindlink/ui/screens/MainScreen.kt
+++ app/src/main/java/com/example/mindlink/ui/screens/MainScreen.kt
@@ -1,28 +1,74 @@
 package com.example.mindlink.ui.screens
 
+import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
 fun MainScreen(
     navigationController: NavController,
 ){
-
+val context = LocalContext.current
     Column(
         modifier = Modifier
-            .fillMaxSize(),
+            .fillMaxSize()
+            .padding(16.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = "Main"
-        )
+
+        Column(
+            modifier = Modifier
+                .background(Color.Cyan),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+
+            Button(
+                onClick = {
+                    Toast.makeText(context, "Clicked", Toast.LENGTH_SHORT).show()
+                }
+            ) {
+                Text(text = "Lesson # 1")
+            }
+            Button(
+                onClick = {},
+                enabled = false,
+                ) {
+                Text(text = "Lesson # 2")
+            }
+            Button(
+                onClick = {},
+                enabled = false,
+            ) {
+                Text(text = "Lesson # 3")
+            }
+            Button(
+                onClick = {},
+                enabled = false,
+            ) {
+                Text(text = "Lesson # 4")
+            }
+            Button(
+                onClick = {},
+                enabled = false,
+            ) {
+                Text(text = "Lesson # 5")
+            }
+
+        }
     }
 
 }
